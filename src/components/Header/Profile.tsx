00b92f633ffbea54ce587aeaf89eed65ecb5aaaf
Marcelo Bueno <marcelorezendebueno@gmail.com>
2021-05-31 20:20:53 -0300

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,23 @@
 import { Flex, Text, Box, Avatar } from '@chakra-ui/react'
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Marcelo Bueno</Text>
-        <Text
-          color="gray.300"
-          fontSize="small"
-        >
-          marcelo.bueno@gmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Marcelo Bueno</Text>
+          <Text
+            color="gray.300"
+            fontSize="small"
+          >
+            marcelo.bueno@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Marcelo Bueno" src="https://github.com/marcelorbueno.png" />
     </Flex>
