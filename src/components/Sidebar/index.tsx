27e012256dc3d7f5737f119038732d65951198ef
Marcelo Bueno <marcelorezendebueno@gmail.com>
2021-06-01 21:39:25 -0300

    Dashboard responsivo

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,22 +1,36 @@
-import { Box, Stack } from "@chakra-ui/react"
-import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri"
+import { Box, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, useBreakpointValue } from "@chakra-ui/react"
 
-import { NavLink } from "./NavLink"
-import { NavSection } from "./NavSection"
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext"
+import { SidebarNav } from "./SidebarNav"
 
 export function Sidebar() {
+  const { isOpen, onClose} = useSidebarDrawer()
+
+  const isDrawerSidebar = useBreakpointValue({
+    base: true,
+    lg: false,
+  })
+
+  if (isDrawerSidebar) {
+    return (
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerContent p="4" bg="rgba(32, 32, 34, 0.95)">
+            <DrawerCloseButton mt="6" />
+            <DrawerHeader>Navegação</DrawerHeader>
+
+            <DrawerBody>
+              <SidebarNav />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    )
+  }
+
   return (
     <Box as="aside" w="64" mr="8">
-      <Stack spacing="12" align="flex-start">
-        <NavSection title="GERAL">
-          <NavLink icon={RiDashboardLine} children="Dashboard" />
-          <NavLink icon={RiContactsLine} children="Usuários" />
-        </NavSection>
-        <NavSection title="AUTOMAÇÃO">
-          <NavLink icon={RiInputMethodLine} children="Formulários" />
-          <NavLink icon={RiGitMergeLine} children="Automação" />
-        </NavSection>
-      </Stack>
+      <SidebarNav />
     </Box>
   )
 }
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -65,41 +65,41 @@ const series = [
 
 export default function Dashboard() {
   return (
-    <Flex direction='column' h='100vh'>
+    <Flex direction="column" h="100vh">
       <Header />
 
-      <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <SimpleGrid flex='1' gap='4' minChildWidth='320px' align='flex-start'>
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
           <Box
-            p={['6', '8']}
-            bg='gray.800'
-            borderRadius='8'
-            pb='4'
+            p={["6", "8"]}
+            bg="gray.800"
+            borderRadius={8}
+            pb="4"
             h="-webkit-fit-content"
           >
-            <Text fontSize='lg' mb='4'>Inscritos da semana</Text>
+            <Text fontSize="lg" mb="4">Inscritos da semana</Text>
             <Chart
               options={options}
               series={series} 
-              type='area'
+              type="area"
               height={160} 
             />
           </Box>
 
           <Box
-            p={['6', '8']}
-            bg='gray.800'
-            borderRadius='8'
-            pb='4'
+            p={["6", "8"]}
+            bg="gray.800"
+            borderRadius={8}
+            pb="4"
             h="-webkit-fit-content"
           >
-            <Text fontSize='lg' mb='4'>Taxa de abertura</Text>
+            <Text fontSize="lg" mb="4">Taxa de abertura</Text>
             <Chart
               options={options}
               series={series} 
-              type='area'
+              type="area"
               height={160} 
             />
           </Box>
