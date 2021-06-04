491d2d4d0bb51f368ffaaac3b2233c39a711841f
Marcelo Bueno <marcelorezendebueno@gmail.com>
2021-06-04 16:22:47 -0300

    Configurando React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,7 @@
-import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue } from "@chakra-ui/react"
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue, Spinner } from "@chakra-ui/react"
 import Link from "next/link"
 import { RiAddLine, RiPencilLine } from "react-icons/ri"
+import { useQuery } from 'react-query'
 
 import { Header } from "../../components/Header"
 import { Pagination } from "../../components/Pagination"
@@ -10,6 +11,13 @@ export default function UserList() {
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
+  })
+
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+
+    return data
   })
 
   return (
@@ -36,113 +44,129 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                { isWideVersion && (
-                  <>
-                    <Th>Data de cadastro</Th>
-                    <Th width="8"></Th>
-                  </>
-                )}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Marcelo Bueno</Text>
-                    <Text fontSize="sm" color="gray.300">marcelo.bueno@gmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && (
-                  <>
-                    <Td>
-                      <Text>28 de Abril de 2021</Text>
+          { isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários.</Text>
+            </Flex>
+          ) : data.users.length === 0 ? (
+            <Flex justify="center">
+              <Text>Não existem usuários cadastrados.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    { isWideVersion && (
+                      <>
+                        <Th>Data de cadastro</Th>
+                        <Th width="8"></Th>
+                      </>
+                    )}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
                     </Td>
                     <Td>
-                      <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine} />}
-                      >
-                        Editar
-                      </Button>
+                      <Box>
+                        <Text fontWeight="bold">Marcelo Bueno</Text>
+                        <Text fontSize="sm" color="gray.300">marcelo.bueno@gmail.com</Text>
+                      </Box>
                     </Td>
-                  </>
-                )}
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Marcelo Bueno</Text>
-                    <Text fontSize="sm" color="gray.300">marcelo.bueno@gmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && (
-                  <>
-                    <Td>
-                      <Text>28 de Abril de 2021</Text>
-                    </Td>
-                    <Td>
-                      <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine} />}
-                      >
-                        Editar
-                      </Button>
-                    </Td>
-                  </>
-                )}
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Marcelo Bueno</Text>
-                    <Text fontSize="sm" color="gray.300">marcelo.bueno@gmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && (
-                  <>
-                    <Td>
-                      <Text>28 de Abril de 2021</Text>
+                    { isWideVersion && (
+                      <>
+                        <Td>
+                          <Text>28 de Abril de 2021</Text>
+                        </Td>
+                        <Td>
+                          <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="purple"
+                            leftIcon={<Icon as={RiPencilLine} />}
+                          >
+                            Editar
+                          </Button>
+                        </Td>
+                      </>
+                    )}
+                  </Tr>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
                     </Td>
                     <Td>
-                      <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine} />}
-                      >
-                        Editar
-                      </Button>
+                      <Box>
+                        <Text fontWeight="bold">Marcelo Bueno</Text>
+                        <Text fontSize="sm" color="gray.300">marcelo.bueno@gmail.com</Text>
+                      </Box>
                     </Td>
-                  </>
-                )}
-              </Tr>
-            </Tbody>
-          </Table>
+                    { isWideVersion && (
+                      <>
+                        <Td>
+                          <Text>28 de Abril de 2021</Text>
+                        </Td>
+                        <Td>
+                          <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="purple"
+                            leftIcon={<Icon as={RiPencilLine} />}
+                          >
+                            Editar
+                          </Button>
+                        </Td>
+                      </>
+                    )}
+                  </Tr>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Marcelo Bueno</Text>
+                        <Text fontSize="sm" color="gray.300">marcelo.bueno@gmail.com</Text>
+                      </Box>
+                    </Td>
+                    { isWideVersion && (
+                      <>
+                        <Td>
+                          <Text>28 de Abril de 2021</Text>
+                        </Td>
+                        <Td>
+                          <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="purple"
+                            leftIcon={<Icon as={RiPencilLine} />}
+                          >
+                            Editar
+                          </Button>
+                        </Td>
+                      </>
+                    )}
+                  </Tr>
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
